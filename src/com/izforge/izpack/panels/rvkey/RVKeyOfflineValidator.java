8fe125dd38001319cc7eb236b08b93c778c09c50
pdaian <phil@linux.com>
2015-11-10 14:21:37 -0600

    allow offline install w checkbox

--- src/com/izforge/izpack/panels/rvkey/RVKeyOfflineValidator.java
+++ src/com/izforge/izpack/panels/rvkey/RVKeyOfflineValidator.java
@@ -4,6 +4,9 @@
 package com.izforge.izpack.panels.rvkey;
 
 import java.util.logging.Logger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static java.nio.file.StandardCopyOption.*;
 
 import com.izforge.izpack.api.data.InstallData;
 import com.izforge.izpack.api.installer.DataValidator;
@@ -20,32 +23,28 @@ import com.runtimeverification.licensing.RVLicenseCache;
  *
  * @author Philip Daian
  */
-public class RVKeyValidator implements DataValidator
+public class RVKeyOfflineValidator implements DataValidator
 {
     private static final Logger logger = Logger.getLogger(IzPanel.class.getName());
 
     @Override
     public Status validateData(InstallData idata) {
-        String installOffline = idata.getVariables().get("installOffline");
-        if (installOffline.equals("yes")) {
-            return Status.OK;
-        }
-
         // Run the RV key algorithm on the IZPack rvKeyEmail, rvKeySecret, and rvProductId variables
-        String email = idata.getVariables().get("rvKeyEmail").toLowerCase().replaceAll("\\s+", "");
-        String password = idata.getVariables().get("rvKeySecret").replaceAll("\\s+", "");
         String productId = idata.getVariables().get("rvProductId");
         String fullProductName = idata.getVariables().get("rvFullProductName");
+        String licenseToInstall = idata.getVariables().get("rvkey.file");
         File licensePath = new File(new File(idata.getVariables().get("INSTALL_PATH")), idata.getVariables().get("rvLicensePath"));
         licensePath.mkdirs();
         Licensing licensingSystem = new Licensing(licensePath, "predict");
         RVLicenseCache licensingCache = licensingSystem.getLicenseCache();
 
-        if (licensingCache.isLicenseCached() && licensingCache.isLicensed()) {
-            return Status.OK;
+        // Try to copy given licensing file to final PATH
+        try {
+            Files.copy((new File(licenseToInstall)).toPath(), (new File(licensePath, "RV_LICENSE")).toPath(), REPLACE_EXISTING);
         }
-
-        licensingCache.fetchLatestLicense(email, password);
+        catch (Exception ex) {
+            return Status.ERROR;
+        }
 
         if (licensingCache.isLicenseCached() && licensingCache.isLicensed()) {
             return Status.OK;
@@ -56,7 +55,7 @@ public class RVKeyValidator implements DataValidator
 
     @Override
     public String getErrorMessageId() {
-        return "Sorry, no valid license found for the provided credentials!  \nPlease check runtimeverification.com/licensing to make sure your account details work and your product license is active and valid.  \nIf you believe this message is in error, "
+        return "Sorry, license file invalid!  \nPlease check runtimeverification.com/licensing to make sure your product license is active and valid.  \nIf you believe this message is in error, "
                 + "please visit https://runtimeverification.com/support.";
     }
 
